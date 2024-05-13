537cffed413b79bd448d473565349c3628478cef
buronda <daniel.buron@storaenso.com>
2024-05-13 13:46:36 +0200

    Demetor violation

---
+++ SWI/DemeterViolation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWI
+{
+    public class DemeterViolation : IDemo
+    {
+        public void RunExample()
+        {
+            House myHouse = new House();
+
+            // Violating the Law of Demeter by accessing a method of an object returned by another method
+            bool isDoorOpen = myHouse.GetDoor().IsOpen();
+
+            Console.WriteLine("Is the door open? " + (isDoorOpen ? "Yes" : "No"));
+        }
+    }
+
+    public class House
+    {
+        private Door door;
+
+        public House()
+        {
+            door = new Door();
+        }
+
+        // Method to return the Door object
+        public Door GetDoor()
+        {
+            return door;
+        }
+    }
+
+    public class Door
+    {
+        private bool isOpen = false;
+
+        public bool IsOpen()
+        {
+            return isOpen;
+        }
+    }
+}
---
+++ SWI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SWI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDemo demo = new DemeterViolation();
+            demo.RunExample();
+        }
+    }
+}
